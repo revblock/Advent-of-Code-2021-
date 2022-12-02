a7603f956c538ad2c6b92064a1bbe78b0f3b2cdf
Revblock <revbl0ck@pm.me>
2022-12-02 10:56:36 +0000

    Improve code

--- day-2/part-2.py
+++ day-2/part-2.py
@@ -1,55 +1,34 @@
-class Play:
-    def __init__(self, opponent, player, score):
-        self.opponent = opponent
-        self.player = player
-        self.score = score
+from functools import reduce
+
+scores = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "X": 0,
+    "Y": 3,
+    "Z": 6
+}
+
+beat_map = {
+    "A": "C",
+    "B": "A",
+    "C": "B"
+}
+
+loss_map = {v: k for k, v in beat_map.items()}
 
 
-class Result:
-    def __init__(self, value, score):
-        self.value = value
-        self.score = score
+def line_score(score, line):
+    opponent, expected = line.strip().split()
+    match expected:
+        case "X":
+            player = beat_map[opponent]
+        case "Y":
+            player = opponent
+        case "Z":
+            player = loss_map[opponent]
+    return score + scores[player] + scores[expected]
 
-
-rock = Play("A", "X", 1)
-paper = Play("B", "Y", 2)
-scissors = Play("C", "Z", 3)
-
-rock.beats = scissors
-paper.beats = rock
-scissors.beats = paper
-
-plays = [rock, paper, scissors]
-
-loss = Result("X", 0)
-draw = Result("Y", 3)
-win = Result("Z", 6)
-
-results = [loss, draw, win]
-
-score = 0
 
 with open("input.txt") as input_file:
-    for line in input_file:
-        opponent, expected = line.strip().split()
-        opponent_play = next(filter(lambda x: x.opponent == opponent, plays))
-        expected_result = next(filter(lambda x: x.value == expected, results))
-        player_play: Play
-        match expected:
-            case "X":
-                player_play = opponent_play.beats
-            case "Y":
-                player_play = opponent_play
-            case "Z":
-                player_play = next(
-                    filter(lambda x: x.beats == opponent_play, plays))
-
-        score += player_play.score
-        if player_play.beats == opponent_play:
-            score += win.score
-        elif player_play == opponent_play:
-            score += draw.score
-        else:
-            score += loss.score
-
-print(score)
+    print(reduce(line_score, input_file, 0))
