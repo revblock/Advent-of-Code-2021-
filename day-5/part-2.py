4e95b24269d860858e1f2e2932f1db0d160a1b57
Revblock <revbl0ck@pm.me>
2022-12-05 12:13:36 +0000

    Day 5

---
+++ day-5/part-2.py
@@ -0,0 +1,53 @@
+import re
+
+stacks = []
+instructions = []
+
+
+with open("input.txt") as input_file:
+    for line in input_file:
+        stripped_line = line.strip("\n")
+
+        if not stripped_line or stripped_line.startswith(" 1"):
+            continue
+
+        if stripped_line.startswith("move"):
+            instructions.append([int(s) for s in re.findall(
+                r'\b\d+\b', stripped_line)])
+            continue
+
+        columns = [stripped_line[idx]
+                   for idx in range(1, len(stripped_line), 4)]
+        columns.reverse()
+
+        if len(stacks) == 0:
+            stacks = [[] for x in range(len(columns))]
+
+        for idx, col in enumerate(columns):
+            if col != " ":
+                stacks[idx].append(col)
+
+stacks.reverse()
+print(stacks)
+
+for instruction in instructions:
+    count, move_from, move_to = instruction
+
+    items_to_move = []
+    for _ in range(count):
+        item = stacks[move_from - 1].pop(0)
+        items_to_move.append(item)
+        # stacks[move_to - 1].insert(0, item)
+
+    items_to_move.reverse()
+
+    for item in items_to_move:
+        stacks[move_to - 1].insert(0, item)
+
+
+result = ""
+for stack in stacks:
+    top = stack.pop(0)
+    result += top
+
+print(result)
